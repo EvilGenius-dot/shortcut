c060ccba6bd6074ea1877e6b3b4c7fd969c84ae5
evilgenius <fuck@fuck.com>
2023-08-02 19:03:14 +0800

    3.0.0

--- Readme/14/install.sh
+++ Readme/14/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="2.0.9"
+VERSION="3.0.0"
 DOWNLOAD_HOST="https://github.com/EvilGenius-dot/shortcut/raw/main/Readme/14/linux"
 ORIGIN_EXEC="gjlink-${VERSION}"
 
--- Readme/8/install.sh
+++ Readme/8/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="2.0.5"
+VERSION="3.0.0"
 DOWNLOAD_HOST="https://github.com/EvilGenius-dot/shortcut/raw/main/Readme/8/linux"
 ORIGIN_EXEC="xxkg-${VERSION}"
 
