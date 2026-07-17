b7d03cb996ce239e5ec98f5372ddc63926ed397b
evilgenius <evilgenius1999@proton.me>
2026-07-17 23:11:19 +0800

    140

---
+++ Readme/140/install.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d8d666574ceaf8a651b512b3d49afcde1359dc92d8db13d79e738e55211c33e9
+size 42370
---
+++ Readme/48/install.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:826c024a6dc3307125c04e1c576787e780060f4b022ae84196b4f34e3577dc90
+size 42408
